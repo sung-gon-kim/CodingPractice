f1b82847cead6057452bceb4c7549ac29e2aaaf8
Sung Gon Kim <sedawk@naver.com>
2022-04-23 17:46:33 +0900

    Adds the run length decode method

--- strings/rle.cpp
+++ strings/rle.cpp
@@ -15,6 +15,24 @@ std::string encode(const std::string& s) {
   return result;
 }
 
+std::string decode(const std::string& s) {
+  int count = 0;
+  std::string result;
+  for (auto c: s) {
+    if (std::isdigit(c)) {
+      count = count * 10 + c - '0';
+    } else {
+      result.append(count, c);
+      count = 0;
+    }
+  }
+  return result;
+}
+
 TEST(RUN_LENGTH, testEncode) {
   EXPECT_EQ("4a1b3c2a", encode("aaaabcccaa"));
+}
+
+TEST(RUN_LENGHT, testDecode) {
+  EXPECT_EQ("aaaabcccaa", decode("4a1b3c2a"));
 }
